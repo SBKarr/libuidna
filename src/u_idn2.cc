296b4f3cda146ed6a984e71f64ecf3eaf0e1f0d2
SBKarr <sbkarr@stappler.org>
2023-01-24 11:16:03 +0800

    Replaced strverscmp with strcmp for android

--- src/u_idn2.cc
+++ src/u_idn2.cc
@@ -151,7 +151,9 @@ extern "C" void idn2_free(void *ptr) {
 }
 
 extern "C" const char* idn2_check_version(const char *req_version) {
-#ifdef __APPLE__
+#if __APPLE__
+	if (!req_version || strcmp(req_version, IDN2_VERSION) <= 0) {
+#elif __ANDROID__
 	if (!req_version || strcmp(req_version, IDN2_VERSION) <= 0) {
 #else
 	if (!req_version || strverscmp(req_version, IDN2_VERSION) <= 0) {
